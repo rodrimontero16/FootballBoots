f6e43e4c089b7b5269b211a47a931b58454dfe83
rodrimontero16 <rodrimontero16@gmail.com>
2023-06-07 09:26:57 -0300

    v0.1.5
    Agregando objetos, arrays y metodos de busqueda y transformacion

--- js/main.js
+++ js/main.js
@@ -20,6 +20,14 @@
 // return precioTotal;
 // }
 
+//misma funcion pero =>
+// const calcularPrecioTotal = () => {
+//     let precioTotal = producto1 + producto2 + producto3 + producto4 + producto5 + producto6;
+//     return precioTotal;
+// }
+
+
+
 // //Armo el ciclo para la eleccion de la compra
 // while (stock != 'f') {
 //     switch (stock) {
@@ -68,65 +76,70 @@
 //     }
 
 
-//Creando un constructor de objeto por cada marca
-class adidas {
-    constructor(modelo, colores, talles, precio){
+// constructor de objetos
+class producto {
+    constructor(marca, modelo, color,talles, precio, id){
+        this.marca = marca.toUpperCase();
         this.modelo = modelo.toUpperCase();
-        this.colores = colores;
+        this.color = color.toUpperCase();
         this.talles = talles;
         this.precio = precio;
-        this.disponible = true; 
+        this.id=id;
+        this.disponible = true;
     }
-    vendido (){
+    productoVendido (){
         this.disponible = false;
     }
 }
-class nike {
-    constructor(modelo, colores, talles, precio){
-        this.modelo = modelo.toUpperCase();
-        this.colores = colores;
-        this.talles = talles;
-        this.precio = precio;
-        this.disponible = true; 
-    }
-    vendido (){
-        this.disponible = false;
-    }
+
+//declaro mi array
+const productos = [
+    new producto ('adidas', 'copa', 'blanco', '39-43', 110000, '0'),
+    new producto ('adidas', 'copa', 'negro', '39-43', 110000, '1'),
+    new producto ('adidas', 'nemeziz', 'naranja', '39-43', 90000, '2'),
+    new producto ('adidas', 'nemeziz', 'blanco', '39-43', 90000, '3'),
+    new producto ('adidas', 'predator', 'blanco y negro', '39-43', 100000, '4'),
+    new producto ('adidas', 'predator', 'negro', '39-43', 95000, '5'),
+    new producto ('adidas', 'predator', 'blanco', '39-43', 125000, '6'),
+    new producto ('adidas', 'predator', 'blanco y naranja', '39-43', 10000, '7'),
+    new producto ('adidas', 'predator', 'negro y rosa', '39-43', 125000, '8'),
+    new producto ('adidas', 'x', 'negro', '39-43', 80000, '9'),
+    new producto ('nike', 'phantom', 'gris', '39-43', 100000, '10'),
+    new producto ('nike', 'mercurial', 'negro', '39-43', 115000, '11'),
+    new producto ('nike', 'mercurial', 'blanco y azul', '39-43', 130000, '12'),
+    new producto ('nike', 'mercurial', 'gris', '39-43', 115000, '13'),
+    new producto ('nike', 'mercurial', 'blanco', '39-43', 135000,  '14'),
+    new producto ('nike', 'mercurial', 'amarillo', '39-43', 90000, '15'),
+    new producto ('nike', 'mercurial', 'naranja', '39-43', 90000, '16'),
+    new producto ('nike', 'mercurial', 'blanco', '39-43', 120000, '17'),
+    new producto ('nike', 'tiempo', 'negro', '39-43', 80000, '18'),
+    new producto ('puma', 'future', 'azul', '39-43', 95000, '19'),
+    new producto ('puma', 'future', 'blanco', '39-43', 95000, '20'),
+    new producto ('puma', 'future', 'amarillo', '39-43', 105000, '21'),
+    new producto ('puma', 'ultra', 'negro y rosa', '39-43', 85000, '22'),
+    new producto ('puma', 'ultra', 'naranja', '39-43', 85000, '23'),
+    new producto ('puma', 'ultra', 'negro y amarillo', '39-43', 85000, '24')
+];
+
+productos[10].productoVendido();
+
+//muestro el array
+console.table (productos);
+
+//practicando metodos de busqueda y transformacion
+
+//primero le pido al usuario para filtrar
+let filtroPedido = prompt ('Busca por marca o modelo').toUpperCase();
+const productosFiltrados = productos.filter ((producto) => (producto.marca.toUpperCase().includes(filtroPedido) || producto.modelo.toUpperCase().includes(filtroPedido)) && producto.disponible === true);
+console.table (productosFiltrados);
+
+//le muestro el resultado de su busqueda y si desea comprar
+let resultadoBusqueda = 'El resultado de su busqueda es:\n';
+productosFiltrados.forEach((producto) => {
+    resultadoBusqueda += `ID: ${producto.id} - ${producto.marca} ${producto.modelo} - Precio: ${producto.precio}\n`;
+});
+if (productosFiltrados.length > 0) {
+    alert (resultadoBusqueda);
+} else {
+    alert('No se encontraron resultados');
 }
-class puma {
-    constructor (modelo, colores, talles, precio){
-        this.modelo = modelo.toUpperCase();
-        this.colores = colores;
-        this.talles = talles;
-        this.precio = precio;
-        this.dispnible = true; 
-    }
-    vendido () {
-        this.disponible = false; 
-    }
-}
-
-// delcaro un array por cada marca
-const BotinesPuma = []; 
-const BotinesAdidas = [];
-const BotinesNike = [];
-
-//agrego objetos a mis arrays
-BotinesPuma.push ( new puma ('Future', 'Azul', '40-42', 85000));
-BotinesPuma.push ( new puma ('Future', 'Blanco', '39-41', 85000));
-BotinesPuma.push ( new puma ('Future', 'Amarillo', '41', 90000));
-BotinesAdidas.push ( new adidas ('Copa', 'Blanco', '41-43', 110000));
-BotinesAdidas.push ( new adidas ('Copa', 'Negro', '42', 110000));
-BotinesNike.push ( new nike ('Phantom', 'Gris', '43', 95000));
-
-// ver los arrays 
-console.log (BotinesAdidas);
-console.log (BotinesPuma);
-console.log (BotinesNike);
-
-// vendo algunos modelos y aplico el metodo vendido
-BotinesAdidas[0].vendido();
-BotinesNike[0].vendido();
-
-// elimino algun objeto de uno de mis arrays 
-BotinesPuma.splice(2,1);
