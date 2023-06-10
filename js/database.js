c0d6ddda693052d9dd45895cd5e20bb66bf2b26a
rodrimontero16 <rodrimontero16@gmail.com>
2023-06-10 19:59:55 -0300

    v0.1.9
    Aplicando JS y mejorando el HTML

---
+++ js/database.js
@@ -0,0 +1,56 @@
+// constructor de objetos
+class producto {
+    constructor(id, marca, modelo, color, precio, foto){
+        this.id=id;
+        this.marca = marca.toUpperCase();
+        this.modelo = modelo.toUpperCase();
+        this.color = color.toUpperCase();
+        this.precio = precio;
+        this.foto = foto; 
+        this.disponible = true;
+    }
+    productoVendido (){
+        this.disponible = false;
+    }
+}
+
+//array de productos
+
+const adidas = [
+    new producto ('1', 'Adidas', 'Copa Sense.1 FG', 'Blanco', 110000, './img/adidas-copa.png'),
+    new producto ('2', 'Adidas', 'Copa Sense FG', 'Negro', 110000, './img/adidas-copa1.png'),
+    new producto ('3', 'Adidas', 'Nemeziz 19.2', 'Naranja', 90000, './img/adidas-nemeziz.png'),
+    new producto ('4', 'Adidas', 'Nemeziz 19.1', 'Blanco y negro', 90000, './img/adidas-nemeziz1.png'),
+    new producto ('5', 'Adidas', 'Predator Mutator 20.1', 'Blanco y negro', 100000, './img/adidas-predator.png'),
+    new producto ('6', 'Adidas', 'Predator EDGE.1 Low FG', 'Negro', 95000, './img/adidas-predator1.png'),
+    new producto ('7', 'Adidas', 'Predator Freak+', 'Blanco', 125000, './img/adidas-predator2.png'),
+    new producto ('8', 'Adidas', 'Predator Mutator 20.1', 'Blanco y naranja', 10000, './img/adidas-predator3.png'),
+    new producto ('9', 'Adidas', 'Predator Freak+', 'Negro y rosa', 125000, './img/adidas-predator4.png'),
+    new producto ('10', 'Adidas', 'X Speedportal', 'Negro', 80000, './img/adidas-x.png'),
+];
+
+const nike = [
+    new producto ('11', 'Nike', 'Hypervenom Phantom III Academy DF FG,', 'Gris', 100000, './img/nike-hyper.png'),
+    new producto ('12', 'Nike', 'Mercurial 360', 'Negro', 115000, './img/nike-mercurial.png'),
+    new producto ('13', 'Nike', 'Zoom Mercurial Vapor 15 Elite CR7 FG', 'Blanco y celeste', 130000, './img/nike-mercurial1.png'),
+    new producto ('14', 'Nike', 'Superfly 8 Academy KM FG/MG', 'Gris', 115000, './img/nike-mercurial2.png'),
+    new producto ('15', 'Nike', 'Superfly 8 Elite+', 'Blanco', 135000, './img/nike-mercurial3.png'),
+    new producto ('16', 'Nike', 'Superfly 6', 'Amarillo', 90000, './img/nike-mercurial4.png'),
+    new producto ('17', 'Nike', 'Superfly 6 Academy MG CR7 Chapter 7', 'Naranja', 90000, './img/nike-mercurial5.png'),
+    new producto ('18', 'Nike', 'Zoom Mercurial Superfly 9 Elite AG-Pro', 'Blanco', 120000, './img/nike-mercurial6.png'),
+    new producto ('19', 'Nike', 'Tiempo Legend 7 Academy FG', 'Negro', 80000, './img/nike-tiempo.png'),
+];
+
+const puma = [
+    new producto ('20', 'Puma', 'Future Ultimate FG/AG', 'Azul', 95000, './img/puma-future.png'),
+    new producto ('21', 'Puma', 'Future 1.4 FG/AG Liberty', 'Blanco', 95000, './img/puma-future1.png'),
+    new producto ('22', 'Puma', 'Future Z 3.1 MXSG', 'Amarillo', 105000, './img/puma-future2.png'),
+    new producto ('23', 'Puma', 'Ultra 1.1 FG/AG', 'Negro y rosa', 85000, './img/puma-ultra.png'),
+    new producto ('24', 'Puma', 'Ultra 1.1 FG/AG', 'Naranja', 85000, './img/puma-ultra1.png'),
+    new producto ('25', 'Puma', 'Ultra 3.2 FG/AG', 'Negro y amarillo', 85000, './img/puma-ultra2.png')
+
+];
+
+console.table (adidas);
+console.table (nike);
+console.table (puma);
--- js/main.js
+++ js/main.js
@@ -1,78 +1,25 @@
-// constructor de objetos
-class producto {
-    constructor(marca, modelo, color,talles, precio, id){
-        this.marca = marca.toUpperCase();
-        this.modelo = modelo.toUpperCase();
-        this.color = color.toUpperCase();
-        this.talles = talles;
-        this.precio = precio;
-        this.id=id;
-        this.disponible = true;
+// accedo a mi div en html por el id
+
+let containerAdidas = document.getElementById('adidasProds');
+let containerNike = document.getElementById('nikeProds');
+let containerPuma = document.getElementById('pumaProds');
+
+// cargando productos
+function renderizarProds (listaprods, containerProds) {
+    for (prod of listaprods){
+        containerProds.innerHTML += `
+        <div class="card m-3" >
+            <img class="card-img-top" src="${prod.foto}" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title"> ${prod.marca} ${prod.modelo}</h5>
+            <p class="card-text"> $ ${prod.precio}</p>
+            <button class="btn btn-primary" id=${prod.id}>Añadir al carrito</button>
+            </div>
+        </div>
+        `;
     }
-    productoVendido (){
-        this.disponible = false;
-    }
-}
+} 
 
-//declaro mi array
-const productos = [
-    new producto ('adidas', 'copa', 'blanco', '39-43', 110000, '0'),
-    new producto ('adidas', 'copa', 'negro', '39-43', 110000, '1'),
-    new producto ('adidas', 'nemeziz', 'naranja', '39-43', 90000, '2'),
-    new producto ('adidas', 'nemeziz', 'blanco', '39-43', 90000, '3'),
-    new producto ('adidas', 'predator', 'blanco y negro', '39-43', 100000, '4'),
-    new producto ('adidas', 'predator', 'negro', '39-43', 95000, '5'),
-    new producto ('adidas', 'predator', 'blanco', '39-43', 125000, '6'),
-    new producto ('adidas', 'predator', 'blanco y naranja', '39-43', 10000, '7'),
-    new producto ('adidas', 'predator', 'negro y rosa', '39-43', 125000, '8'),
-    new producto ('adidas', 'x', 'negro', '39-43', 80000, '9'),
-    new producto ('nike', 'phantom', 'gris', '39-43', 100000, '10'),
-    new producto ('nike', 'mercurial', 'negro', '39-43', 115000, '11'),
-    new producto ('nike', 'mercurial', 'blanco y azul', '39-43', 130000, '12'),
-    new producto ('nike', 'mercurial', 'gris', '39-43', 115000, '13'),
-    new producto ('nike', 'mercurial', 'blanco', '39-43', 135000,  '14'),
-    new producto ('nike', 'mercurial', 'amarillo', '39-43', 90000, '15'),
-    new producto ('nike', 'mercurial', 'naranja', '39-43', 90000, '16'),
-    new producto ('nike', 'mercurial', 'blanco', '39-43', 120000, '17'),
-    new producto ('nike', 'tiempo', 'negro', '39-43', 80000, '18'),
-    new producto ('puma', 'future', 'azul', '39-43', 95000, '19'),
-    new producto ('puma', 'future', 'blanco', '39-43', 95000, '20'),
-    new producto ('puma', 'future', 'amarillo', '39-43', 105000, '21'),
-    new producto ('puma', 'ultra', 'negro y rosa', '39-43', 85000, '22'),
-    new producto ('puma', 'ultra', 'naranja', '39-43', 85000, '23'),
-    new producto ('puma', 'ultra', 'negro y amarillo', '39-43', 85000, '24')
-];
-
-productos[4].productoVendido();
-productos[10].productoVendido();
-
-//muestro el array
-console.table (productos);
-
-//practicando metodos de busqueda y transformacion
-
-//primero le pido al usuario para filtrar
-let filtroPedido = prompt ('Busca por marca o modelo').toUpperCase();
-const productosFiltrados = productos.filter ((producto) => (producto.marca.toUpperCase().includes(filtroPedido) || producto.modelo.toUpperCase().includes(filtroPedido)) && producto.disponible === true);
-
-//le muestro el resultado de su busqueda y si desea comprar
-let resultadoBusqueda = 'El resultado de su busqueda es:\n';
-productosFiltrados.forEach((producto) => {
-    resultadoBusqueda += `ID: ${producto.id} - ${producto.marca} ${producto.modelo} - Precio: ${producto.precio}\n`;
-});
-if (productosFiltrados.length > 0) {
-    let adquirirProducto = prompt (resultadoBusqueda+ '\n ¿Desea adquirir alguno de estos productos? Seleccione el ID o F para finalizar');
-        if (adquirirProducto.toUpperCase() == 'F') {
-            alert('Gracias por visitarnos.');
-        } else {
-            const productoSeleccionado = productosFiltrados.find((producto) => producto.id == (adquirirProducto));
-            if (productoSeleccionado) {
-                const resultadoCompra = `Usted ha adquirido:\n${productoSeleccionado.marca} ${productoSeleccionado.modelo} por un precio de $${productoSeleccionado.precio}. ¡Gracias por su compra!`;
-                alert(resultadoCompra);
-            } else {
-                alert('El ID seleccionado no es válido');
-            }
-    }
-} else {
-    alert('No se encontraron resultados');
-}
+renderizarProds(adidas, containerAdidas);
+renderizarProds(nike, containerNike);
+renderizarProds(puma, containerPuma);
