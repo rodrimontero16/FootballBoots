c0f0992e1b124d03160b75868637e3f942366c06
rodrimontero16 <rodrimontero16@gmail.com>
2023-06-15 21:50:20 -0300

    v0.2.3
    Solucionado problema del boton

--- js/logic.js
+++ js/logic.js
@@ -4,17 +4,12 @@ let containerAdidas = document.getElementById('adidasProds');
 let containerNike = document.getElementById('nikeProds');
 let containerPuma = document.getElementById('pumaProds');
 
-
-//filtro el array por la marca
-let productosAdidas = productos.filter ((producto) => producto.marca === 'ADIDAS');
-let productosNike = productos.filter ((producto) => producto.marca === 'NIKE');
-let productosPuma = productos.filter ((producto) => producto.marca === 'PUMA');
-
 // cargando productos
-function renderizarProds (listaprods, containerProds) {
+function renderizarProds (productos) {
 
-    for (prod of listaprods){
-        containerProds.innerHTML += `
+    for (prod of productos){
+        if (prod.marca === 'ADIDAS'){
+        containerAdidas.innerHTML += `
         <div class="card m-3" >
             <img class="card-img-top" src="${prod.foto}" alt="Card image cap">
             <div class="card-body">
@@ -24,6 +19,29 @@ function renderizarProds (listaprods, containerProds) {
             </div>
         </div>
         `;
+    } else if (prod.marca === 'NIKE'){
+        containerNike.innerHTML += `
+        <div class="card m-3" >
+            <img class="card-img-top" src="${prod.foto}" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title"> ${prod.marca} ${prod.modelo}</h5>
+            <p class="card-text"> $ ${prod.precio}</p>
+            <button class="btn btn-primary addCarrito" id=${prod.id}>Añadir al carrito</button>
+            </div>
+        </div>
+        `;
+    } else if (prod.marca === 'PUMA'){
+        containerPuma.innerHTML += `
+        <div class="card m-3" >
+            <img class="card-img-top" src="${prod.foto}" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title"> ${prod.marca} ${prod.modelo}</h5>
+            <p class="card-text"> $ ${prod.precio}</p>
+            <button class="btn btn-primary addCarrito" id=${prod.id}>Añadir al carrito</button>
+            </div>
+        </div>
+        `;
+    }
     }
 
     // tomo los botones
@@ -36,8 +54,7 @@ function renderizarProds (listaprods, containerProds) {
         })
     }
 }
-// llamo los productos y los pongo en el div que le corresponde
-renderizarProds (productosAdidas, containerAdidas);
-renderizarProds (productosNike, containerNike);
-renderizarProds (productosPuma, containerPuma);
+
+// llamo la funcion 
+renderizarProds (productos);
 
