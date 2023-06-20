d29e68862c7dfddb25c3e07fa2b2ec3b50cd3e21
rodrimontero16 <rodrimontero16@gmail.com>
2023-06-20 18:42:45 -0300

    v0.2.5
    Mejorando el carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,7 @@
 //tomo el tablabody
 let tablaBody = document.getElementById('tablabody');
+let totalCompra = 0;
+let totalCarrito = document.getElementById ('totalCompra');
 
 //traigo lo que tengo guardado en el ls
 const carritoEnLS = JSON.parse(localStorage.getItem('carrito'))
@@ -8,16 +10,23 @@ console.table(carritoEnLS);
 // Función para renderizar el carrito
 function renderizarCarrito(prods) {
     for (const prods of carritoEnLS) {
+        const subtotalProducto = prods.precio;
+        totalCompra += subtotalProducto;
+
         tablaBody.innerHTML += `
         <tr>
             <td>${prods.marca} ${prods.modelo}</td>
-            <td>${prods.precio}</td>
+            <td>$${subtotalProducto.toLocaleString('es-ES')}</td>
             <td>1</td>
-            <td id="subtotal>${prods.precio}</td>
+            <td class="subtotal">$${subtotalProducto.toLocaleString('es-ES')}</td>
         </tr>
     `;
     }
 }
 
-//renderizo los productos en el carrito
+// Renderizar los productos en el carrito
 renderizarCarrito(carritoEnLS);
+
+// Actualizar el total
+totalCarrito.innerText = '$' + totalCompra.toLocaleString('es-ES');
+
