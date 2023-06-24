ea67c2002788e549ff4688642dfd1809ad685b95
rodrimontero16 <rodrimontero16@gmail.com>
2023-06-24 16:22:28 -0300

    v0.3.3
    Mejorando estilo de la pagina

--- js/carrito.js
+++ js/carrito.js
@@ -5,8 +5,13 @@ let totalCarrito = document.getElementById ('totalCompra');
 // declaro la variable del total de la compra
 let totalCompra;
 
-// //traigo lo que tengo guardado en el localstorage
+//traigo lo que tengo guardado en el localstorage
 carrito = cargarCarritoLS();
 
 // Cargo los productos al carrito
 renderizarCarrito(carrito);
+
+//IDEAS
+//En el carrito a la celda cantidad hacerle una funcion que me vaya diciendo cuantas veces hice click en el boton de un determinado producto y que por cada vez que haga click me sume uno y eso me lo multiplique por el prod precio para tener el subtotal
+
+// con cantidad.value puedo multriplicar por el valor que tenga el input
